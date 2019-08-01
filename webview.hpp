8cc05db807a82a41bde3c71a1ace793b109d5634
Michael Kim <micklepickle98@gmail.com>
2019-08-01 00:19:54 -0700

    Add window sizing for Windows

--- webview.hpp
+++ webview.hpp
@@ -116,6 +116,7 @@ private:
   WebViewControl webview{nullptr};
   MSG msg; // Message from main loop
 
+  void resize();
   static LRESULT CALLBACK WndProcedure(HWND hwnd, UINT msg, WPARAM wparam,
                                        LPARAM lparam);
 #elif defined(WEBVIEW_MAC) // WEBVIEW_WIN
@@ -142,6 +143,7 @@ private:
   static void webview_load_changed_cb(WebKitWebView *webview,
                                       WebKitLoadEvent event, gpointer arg);
   static void destroyWindowCb(GtkWidget *widget, gpointer arg);
+  // static gboolean closeWebViewCb(WebKitWebView *webView, GtkWidget *window);
   static gboolean webview_context_menu_cb(WebKitWebView *webview,
                                           GtkWidget *default_menu,
                                           WebKitHitTestResult *hit_test_result,
@@ -171,13 +173,6 @@ int WebView::init() {
     return -1;
   }
 
-  if (debug) {
-    AllocConsole();
-    FILE *out, *err;
-    freopen_s(&out, "CONOUT$", "w", stdout);
-    freopen_s(&err, "CONOUT$", "w", stderr);
-  }
-
   // Initialize Win32 window
   WNDCLASSEX wc;
   wc.cbSize = sizeof(WNDCLASSEX);
@@ -204,6 +199,16 @@ int WebView::init() {
     return 0;
   }
 
+  // Set window size
+  RECT r;
+  r.left = 0;
+  r.top = 0;
+  r.right = width;
+  r.bottom = height;
+  SetWindowPos(hwnd, NULL, r.left, r.top, r.right - r.left, r.bottom - r.top,
+               SWP_NOZORDER | SWP_NOACTIVATE | SWP_FRAMECHANGED);
+
+  // Used with GetWindowLongPtr
   SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)this);
 
   ShowWindow(hwnd, SW_SHOWDEFAULT);
@@ -231,11 +236,8 @@ int WebView::init() {
   webview.NavigationStarting(
       [=](auto &&, auto &&) { webview.AddInitializeScript(inject); });
 
-  // Set window bounds
-  RECT r;
-  GetClientRect(hwnd, &r);
-  Rect bounds(r.left, r.top, r.right - r.left, r.bottom - r.top);
-  webview.Bounds(bounds);
+  // Set webview bounds
+  resize();
 
   webview.IsVisible(true);
 
@@ -323,12 +325,28 @@ void WebView::css(std::wstring css) {
 
 void WebView::exit() { PostQuitMessage(WM_QUIT); }
 
+void WebView::resize() {
+  RECT rc;
+  GetClientRect(hwnd, &rc);
+  Rect bounds((float)rc.left, (float)rc.top, (float)(rc.right - rc.left),
+              (float)(rc.bottom - rc.top));
+  webview.Bounds(bounds);
+}
+
 LRESULT CALLBACK WebView::WndProcedure(HWND hwnd, UINT msg, WPARAM wparam,
                                        LPARAM lparam) {
   WebView *w =
       reinterpret_cast<WebView *>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
 
   switch (msg) {
+  case WM_SIZE:
+    // WM_SIZE will first fire before the webview finishes loading
+    // init() calls resize(), so this call is only for size changes
+    // after fully loading.
+    if (w != nullptr && w->init_done) {
+      w->resize();
+    }
+    return DefWindowProc(hwnd, msg, wparam, lparam);
   case WM_CLOSE:
     DestroyWindow(hwnd);
     break;
@@ -698,7 +716,7 @@ void WebView::destroyWindowCb(GtkWidget *widget, gpointer arg) {
   static_cast<WebView *>(arg)->exit();
 }
 
-// static gboolean closeWebViewCb(WebKitWebView *webView, GtkWidget *window) {
+// gboolean WebView::closeWebViewCb(WebKitWebView *webView, GtkWidget *window) {
 //   gtk_widget_destroy(window);
 //   return TRUE;
 // }
