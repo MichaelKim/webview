ca3869421633467443310835f4a4ff5aaa36c45a
Michael Kim <micklepickle98@gmail.com>
2021-07-03 18:29:59 -0400

    Fix callback.cpp example for WEBVIEW_EDGE

--- examples/callback/callback.cpp
+++ examples/callback/callback.cpp
@@ -5,42 +5,40 @@
 
 #include "webview.hpp"
 
-#ifdef WEBVIEW_WIN
+#if defined(WEBVIEW_WIN) || defined(WEBVIEW_EDGE)
 #define to_string std::to_wstring
 #else
 #define to_string std::to_string
 #endif
 
-long long factorial(long long n) {
-  return n <= 1 ? 1 : n * factorial(n - 1);
-}
+long long factorial(long long n) { return n <= 1 ? 1 : n * factorial(n - 1); }
 
-void callback(wv::WebView &w, std::string &arg) {
-  try {
-    long long num = std::stoll(arg);
-    w.eval(Str("result(") + to_string(factorial(num)) + Str(")"));
-  } catch (std::exception e) {
-    w.eval(Str("result('Invalid number')"));
-  }
+void callback(wv::WebView &w, wv::String &arg) {
+    try {
+        long long num = std::stoll(arg);
+        w.eval(Str("result(") + to_string(factorial(num)) + Str(")"));
+    } catch (std::exception e) {
+        w.eval(Str("result('Invalid number')"));
+    }
 }
 
 WEBVIEW_MAIN {
-  wv::WebView w{800,
-                600,
-                true,
-                true,
-                Str("WebView Callback"),
-                Str("http://localhost:8080")};
+    wv::WebView w{800,
+                  600,
+                  true,
+                  true,
+                  Str("WebView Callback"),
+                  Str("http://localhost:8080")};
 
-  // This can be called before or after w.init();
-  w.setCallback(callback);
+    // This can be called before or after w.init();
+    w.setCallback(callback);
 
-  if (w.init() == -1) {
-    return 1;
-  }
+    if (w.init() == -1) {
+        return 1;
+    }
 
-  while (w.run() == 0)
-    ;
+    while (w.run() == 0)
+        ;
 
-  return 0;
+    return 0;
 }
