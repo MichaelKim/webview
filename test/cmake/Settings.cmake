7a9356bc5b7a63f6f42b7d8351e9e10383f04a4a
Michael Kim <micklepickle98@gmail.com>
2021-09-27 17:37:06 -0400

    Enable sanitizers for testing

--- test/cmake/Settings.cmake
+++ test/cmake/Settings.cmake
@@ -17,9 +17,10 @@ endif()
 # Generate compile_commands.json to make it easier to work with clang based tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" OFF)
+option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" ON)
 
 if(ENABLE_IPO)
+  cmake_policy(SET CMP0069 NEW)
   include(CheckIPOSupported)
   check_ipo_supported(
     RESULT
